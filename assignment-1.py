8d2a8547a5c5d783f48c8ea0d0ad114e686a09dd
Ian DubaiDeveloper <ian.dubaideveloper@yahoo.com>
2023-02-15 23:22:54 -0800

    assignment-1

--- assignment-1.py
+++ assignment-1.py
@@ -42,6 +42,26 @@ for vehicle in vehicles:
 
 print(f"Total weight of all vehicles: {Vehicle.total_weight(vehicles)} kg")
 
+"""
+Create a class Vehicle with the following attributes:
+
+make (string)
+model (string)
+year (integer)
+weight (float)
+Also, define the following methods in the class:
+
+description: which returns a string in the following format: "{make} {model} ({year}), weight: {weight} kg"
+total_weight: which returns the sum of the weight attribute of all Vehicle objects that have been created
+Create two subclasses of Vehicle: Car and Truck. The Car class should have an additional attribute num_doors (integer) and the Truck class should have an additional attribute payload_capacity (float).
+
+Override the description method in both subclasses to include information about their respective additional attributes.
+
+Create a list of 5 Vehicle objects, containing at least two objects of each subclass, and call the description method on each object.
+
+Also, demonstrate the use of the total_weight method to print the total weight of all vehicles that have been created
+"""
+
 
 
 
